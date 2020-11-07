5015ee9ed3318c17304c40df587f849b70626751
Colten Webb <coltenwebb@gmail.com>
2020-11-07 17:41:41 -0600

    bundling improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,22 @@
-var webpack = require("webpack");
-var path = require("path");
-var PROD = (process.env.NODE_ENV === "production")
-var LEGACY = (process.env.LEGACY)
-var MINIMIZE = (process.env.MINIMIZE === "true")
-var hostname = "localhost";
-var port = 8080;
+const path = require("path");
+const PROD = process.env.NODE_ENV === "production";
+const LEGACY = process.env.LEGACY === "true";
+const MINIMIZE = process.env.MINIMIZE === "true";
+const hostname = "localhost";
+const port = 8080;
 
-var filename = "[name]";
-var sourceMapFilename = "[name]";
-if (LEGACY) {
-	filename += ".legacy";
-}
-if (MINIMIZE) {
-	filename += ".min.js";
-	sourceMapFilename += ".min.js.map";
-} else {
-	filename += ".js";
-	sourceMapFilename += ".js.map";
-}
+let filename = "[name]";
+if (LEGACY) filename += ".legacy";
+if (MINIMIZE) filename += ".min";
+filename += ".js";
+let sourceMapFilename = filename + ".map";
 
 module.exports = {
 	mode: process.env.NODE_ENV,
 	entry: {
-		"epub": "./src/epub.js",
+		epub: "./src/epub.js",
 	},
-	devtool: MINIMIZE ? false : 'source-map',
+	devtool: MINIMIZE ? false : "source-map",
 	output: {
 		path: path.resolve("./dist"),
 		filename: filename,
@@ -32,20 +24,20 @@ module.exports = {
 		library: "ePub",
 		libraryTarget: "umd",
 		libraryExport: "default",
-		publicPath: "/dist/"
+		publicPath: "/dist/",
 	},
 	optimization: {
-		minimize: MINIMIZE
+		minimize: MINIMIZE,
 	},
 	externals: {
 		"jszip/dist/jszip": "JSZip",
-		"xmldom": "xmldom"
+		xmldom: "xmldom",
 	},
 	plugins: [],
 	resolve: {
 		alias: {
-			path: "path-webpack"
-		}
+			path: "path-webpack",
+		},
 	},
 	devServer: {
 		host: hostname,
@@ -54,8 +46,8 @@ module.exports = {
 		headers: {
 			"Access-Control-Allow-Origin": "*",
 			"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
-			"Access-Control-Allow-Headers": "Content-Type"
-		}
+			"Access-Control-Allow-Headers": "Content-Type",
+		},
 	},
 	module: {
 		rules: [
@@ -65,19 +57,26 @@ module.exports = {
 				use: {
 					loader: "babel-loader",
 					options: {
-						presets: [["@babel/preset-env", {
-							targets: LEGACY ? "defaults" : "last 2 Chrome versions, last 2 Safari versions, last 2 ChromeAndroid versions, last 2 iOS versions, last 2 Firefox versions, last 2 Edge versions",
-							corejs: 3,
-							useBuiltIns: "usage",
-							bugfixes: true,
-							modules: false
-						}]]
-					}
-				}
-			}
-		]
+						presets: [
+							[
+								"@babel/preset-env",
+								{
+									targets: LEGACY
+										? "defaults"
+										: "last 2 Chrome versions, last 2 Safari versions, last 2 ChromeAndroid versions, last 2 iOS versions, last 2 Firefox versions, last 2 Edge versions",
+									corejs: 3,
+									useBuiltIns: "usage",
+									bugfixes: true,
+									modules: false,
+								},
+							],
+						],
+					},
+				},
+			},
+		],
 	},
 	performance: {
-		hints: false
-	}
-}
+		hints: false,
+	},
+};
